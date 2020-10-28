8323a1d446ec6e2aee4375e54e71df3023c4dff1
[InnCreator] <[engbethany4@gmail.com]>
2020-10-28 15:55:39 +0300

    update on lesson 2 assignment

--- lesson2 assignment.py
+++ lesson2 assignment.py
@@ -1,14 +1,15 @@
 person = {'john':40, 'peter':45} 
-# print(person[susan])
-
-year = 1980
-
+print(person[susan])
+# c) Since “susan” is not a key in the set, Python raises a KeyError exception
+#Output will be undefined as Susan is not part of the tuple components
 
 #Question 2: loops
+#Create a while loop to print from 1980 to 2020
+year = 1980
 while year <= 2020:
     print(year)
     year += 1
-
-for num in range(-20):
+# Create a for loop to print from -20 to +20
+for num in range(-20, 21):
     print(num)
 
