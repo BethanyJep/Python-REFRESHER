1a283e92347e54a9a9e1d8143ac85282fc140b16
[InnCreator] <[engbethany4@gmail.com]>
2020-10-29 10:01:57 +0300

    try and except

---
+++ lesson3.py
@@ -0,0 +1,11 @@
+person = {'john':40, 'peter':45} 
+try:
+    print(person['peter'])
+except:
+    print('something unusual')
+
+List = ['Oranges', 'Mangoes']
+try:
+    print(List[2])
+except:
+    print('something unusual')
